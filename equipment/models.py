fb06c866980bf9c886cd6cd412fc8bfd80755643
Doralitze <doralitze@chaotikum.org>
2020-05-13 14:47:23 +0200

    chg: Instead of containers and items only have items

--- equipment/models.py
+++ equipment/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from PIL import Image
@@ -19,6 +20,9 @@ class Photograph(models.Model):
 
     def __str__(self):
         return "Image '{}' uploaded by {}".format(self.title, str(self.uploaded_by))
+
+    class Meta:
+        ordering = ["uploaded_at"]
 
     def save(self, *args, **kwargs):
         if self.full_resolution_image == None:
@@ -45,7 +49,7 @@ class Photograph(models.Model):
         image.save(str(self.image.path), 'PNG')
 
 
-class Container(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=100, help_text="The name of the container")
     notes = models.TextField(blank=True, help_text="Thoughts on the container")
     visible_to_others = models.BooleanField(default=True, help_text="If this is set to false "\
@@ -54,13 +58,13 @@ class Container(models.Model):
     parent_container = models.ForeignKey("self", null=True, default=None, on_delete=models.SET_NULL)
     images = models.ManyToManyField(Photograph)
     created_at = models.DateField(editable=False, default=datetime.date.today, help_text="Date of creation")
-
-
-class Item(models.Model):
-    name = models.CharField(max_length=100, help_text="The name of the item")
-    notes = models.TextField(blank=True, help_text="Thoughts on the item (markdown)")
-    container = models.ForeignKey(Container, null=True, blank=True, default=None, on_delete=models.SET_NULL)
-    images = models.ManyToManyField(Photograph)
     owner = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
-    created_at = models.DateField(editable=False, default=datetime.date.today, help_text="Date of creation")
+    is_container = models.BooleanField(default=False, help_text="If true this item can contain other items.")
+    can_be_lend_alone = models.BooleanField(default=True, help_text="If false one can only lend the parent container.")
+
+    def save(self, *args, **kwargs):
+        if self.parent_container != None:
+            if not self.parent_container.is_container:
+                raise ValidationError("The selected parent isn't a container")
+        super().save(*args, **kwargs)
 
--- peers/models.py
+++ peers/models.py
@@ -15,9 +15,19 @@ class PeerGroup(models.Model):
     visible_to_strangers = models.BooleanField(default=False, help_text="If true one can apply for becomming a member")
     applications = models.ManyToManyField(User, through="GroupApplication")
 
+    class Meta:
+        ordering = ["name", "created_at"]
+
 class GroupApplication(models.Model):
     applicant = models.ForeignKey(User, null=False)
     application_for = models.ForeignKey(PeerGroup, null=False)
     approved_by = models.ManyToManyField(User, help_text="Once all admins of a group approved the application, "\
             "the last approving admin can fulfill it.")
+    applied_at = models.DateField(editable=False, default=datetime.date.today, help_text="Date of application")
+    declined = models.BooleanField(default=False, help_text="A declined application won't be deleted."\
+            " Once an application was declined the user can't reapply. A declined application can be reenabled.")
+    declined_by = models.ForeignKey(User, null=True, default=None)
+
+    class Meta:
+        ordering = ["declined", "applied_at"]
 
