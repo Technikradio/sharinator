7c9c6198a037228c98450a816881c0b7159fd545
Doralitze <doralitze@chaotikum.org>
2020-05-14 14:32:06 +0200

    add: administration app and user profile model

--- ROADMAP.md
+++ ROADMAP.md
@@ -8,6 +8,10 @@ This document provides a feature roadmap.
 
 ## Version 0.3
  * Implement NGINX secure link for uploads and make it optional via the ´´´settings.py´´´ file
+ * Add geographic information (Where the home of an item is and where its lending will be located)
+   to the database.
+ * Add a feature to find the current position of an item (using the home and lending data and
+   a given or current ime stamp)
 
 ## Version 0.2
  * Extensive admin interface
---
+++ administration/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+from sharinator.equipment.models import Photograph
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    landline_number = PhoneNumberField(blank=True)
+    mobile_number = PhoneNumberField(blank=True)
+    additional_number = PhoneNumberField(blank=True)
+    phone_numbers_visible = models.BooleanField(default=True,
+            help_text="Should the phone numbers be visible to people who I leaded stuff to or from")
+    profile_picture = models.ForeignKey(Photograph, null=True)
+    pgp_key_id = models.TextField(blank=True, help_text="The key id to identify a pgp key for optional mail encryption.")
+
