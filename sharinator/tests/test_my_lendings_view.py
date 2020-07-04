f622302995b6deb73f16c0d779d5dba205cb0f51
Doralitze <doralitze@chaotikum.org>
2020-07-04 14:55:17 +0200

    fix: lending view unit test password gen

--- sharinator/tests/test_my_lendings_view.py
+++ sharinator/tests/test_my_lendings_view.py
@@ -10,30 +10,40 @@ from sharinator.equipment.models import Item
 from sharinator.shares.models import Lending
 from sharinator.shares.views.lend_views import MyLendsView
 
-class MyLendingsViewTestCase(TestCase):
 
+class MyLendingsViewTestCase(TestCase):
     def setUp(self):
         u: User = User.objects.create_user(username="lendingtestuser", password="1234")
+        u.set_password("1234")
+        u.save()
         for i in range(10):
             item = Item.objects.create(name="test item " + str(i), owner=u)
-            Lending.objects.create(lending_user=u, item_to_lend=item, \
-                    start_of_lending=datetime.date.today() + datetime.timedelta(days=1), \
-                    end_of_lending=datetime.date.today() + datetime.timedelta(days=2))
+            Lending.objects.create(
+                lending_user=u,
+                item_to_lend=item,
+                start_of_lending=datetime.date.today() + datetime.timedelta(days=1),
+                end_of_lending=datetime.date.today() + datetime.timedelta(days=2),
+            )
         item = Item.objects.create(name="The other test item", owner=u)
-        Lending.objects.create(lending_user=u, item_to_lend=item, \
-                start_of_lending=datetime.date.today() + datetime.timedelta(days=5), \
-                end_of_lending=datetime.date.today() + datetime.timedelta(days=6))
+        Lending.objects.create(
+            lending_user=u,
+            item_to_lend=item,
+            start_of_lending=datetime.date.today() + datetime.timedelta(days=5),
+            end_of_lending=datetime.date.today() + datetime.timedelta(days=6),
+        )
 
     def test_context_generation(self):
-        l = self.client.login(username='lendingtestuser', password='1234')
+        l = self.client.login(username="lendingtestuser", password="1234")
         response = self.client.get(reverse("mylends"), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertFalse(response.context["more_future_lendings"])
         self.assertEqual(len(response.context["future_lend_groups"]), 2)
         self.assertEqual(len(response.context["future_lend_groups"][0]), 10)
         self.assertEqual(len(response.context["future_lend_groups"][1]), 1)
-        self.assertEqual(response.context["future_lend_groups"][1][0].item_to_lend.name, "The other test item")
+        self.assertEqual(
+            response.context["future_lend_groups"][1][0].item_to_lend.name,
+            "The other test item",
+        )
 
     def test_correct_user_data(self):
-        pass # TODO implement using self.client.get
-
+        pass  # TODO implement using self.client.get
